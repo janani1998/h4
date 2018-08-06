dea6895d7fa788e75a832ad838390996379229e0
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-08-06 17:53:05 +0530

    Create sqrt.java

---
+++ sqrt.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n1=2,n2=8;
+		int n=n1*n2;
+		int a=(int)Math.sqrt(n);
+		if((a*a)==n)
+		System.out.print("Yes");
+		else
+		System.out.print("No");
+		
+	}
+}
